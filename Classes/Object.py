162150d840e78f2e6f2f16a4dfd98faa0be20fca
DeriOr1907 <101052097+DeriOr1907@users.noreply.github.com>
2022-03-16 19:55:02 +0200

    new class: Character

--- Classes/Object.py
+++ Classes/Object.py
@@ -1,2 +1,5 @@
 class Object:
-    def __init__(self):
+    def __init__(self,Location,height,width):
+        self.Location = Location
+        self.height = height
+        self.width = width
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 pygame.init()
-screen_size = (984,553)
+screen_size = (1000,530)
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption('Elements')
 clock = pygame.time.Clock()
-img = pygame.transform.scale(pygame.image.load(""),(984,553))
+background = pygame.transform.scale(pygame.image.load("Images/background2.jpeg"), screen_size)
 while True: #Caharater1 is alive and Caracter2 is alive:
-    screen.blit(img,(0, 0))
+    screen.blit(background, (0, 0))
+    pygame.display.update()
+    clock.tick(60)
 pygame.quit()
 quit()
