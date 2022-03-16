74fa5820e6c00693ebc42f6bbb24d79363be9065
DeriOr1907 <101052097+DeriOr1907@users.noreply.github.com>
2022-03-16 16:28:51 +0200

    new class: Character

--- Classes/Character.py
+++ Classes/Character.py
@@ -1,2 +1,6 @@
 class Character:
     def __init__(self,Location,Gender,Color,direction,Img):
+        self.Location = Location
+        self.Gender = Gender
+        self.Color = Color
+        self.
