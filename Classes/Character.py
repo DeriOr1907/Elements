393dbfb7717c6a962eb17e5d2e759fd0b74f9435
DeriOr1907 <101052097+DeriOr1907@users.noreply.github.com>
2022-03-19 20:47:05 +0200

    photos of both sides

--- Classes/Character.py
+++ Classes/Character.py
@@ -1,8 +1,12 @@
+from main import screen
 class Character:
-    def __init__(self,Location,Gender,Color,Img,):
+    def __init__(self,Location,Color,Img):
         self.Location = Location
-        self.Gender = Gender
-        self.Color = Color
-        self.Direction = 0 # -1 is left 0 is straight 1 is right
-        self.IMG = Img
-        self.Alive =True
+        self.color = Color
+        self.direction = 0 # 0 is forward, 1 is right and 2 is left
+        self.images = Img
+        self.alive = True
+
+    def display_character(self):
+        if self.alive:
+            screen.blit(self.images[self.direction], self.Location)
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import pygame
+from Classes.Character import *
 pygame.init()
 screen_size = (1000,530)
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption('Elements')
 clock = pygame.time.Clock()
-img = pygame.transform.scale(pygame.image.load("Images/BlueGRun.PNG"), (45,45))
+
+
+def create_images_list(imgpath1,imgpath2,imgpath3):
+    imags = [pygame.transform.scale(pygame.image.load(imgpath1), (45,45)), pygame.transform.scale(pygame.image.load(imgpath2), (45,45)), pygame.transform.scale(pygame.image.load(imgpath3), (45,45))]
+    return imags
+
+
+bluegirl_images = create_images_list("Images/Bluegirl.png", "Images/BlueGRun.PNG", "Images/BlueGRunLeft.PNG")
+bluegirl = Character((300,200),"Blue",bluegirl_images)
 background = pygame.transform.scale(pygame.image.load("Images/background2.jpeg"), screen_size)
-while True: #Caharater1 is alive and Caracter2 is alive:
+while True: # Caharater1 is alive and Caracter2 is alive:
     screen.blit(background, (0, 0))
-    screen.blit(img, (300, 200))
+    bluegirl.display_character()
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
