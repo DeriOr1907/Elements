57a722500abaa400fc3ece9434fb9b5bda21f583
DeriOr1907 <101052097+DeriOr1907@users.noreply.github.com>
2022-03-29 20:54:50 +0300

    fixed object class and added some objects

--- Classes/Character.py
+++ Classes/Character.py
@@ -1,4 +1,5 @@
 from Constants import screen, gravity
+from Classes.Object import *
 class Character:
     def __init__(self,Location, Color, Img):
         self.Location = Location
@@ -19,10 +20,12 @@ class Character:
                 self.jumpV = self.jumpV-0.5
         if self.moving_right:
             self.direction = 1
-            self.Location = (self.Location[0] + 2, self.Location[1])
+            if self.able_to_move_right():
+                self.Location = (self.Location[0] + 2, self.Location[1])
         if self.moving_left:
             self.direction = 2
-            self.Location = (self.Location[0] - 2, self.Location[1])
+            if self.able_to_move_left():
+                self.Location = (self.Location[0] - 2, self.Location[1])
         if self.alive:
             self.Location = (self.Location[0], self.Location[1] + gravity - self.jumpV)
             screen.blit(self.images[self.direction], self.Location)
@@ -46,8 +49,15 @@ class Character:
     def move_left(self):
         self.moving_left = True
 
-    def able_to_move_right(self):
-        pass
+    def able_to_move_right(self, objects):
+        uploc = (self.Location[0]+40,self.Location[1])
+        for obj in objects:
+            if True:
+                pass
+        return True
+
+    def able_to_move_left(self, objects):
+        return True
 
 
 
