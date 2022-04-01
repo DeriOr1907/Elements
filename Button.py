ba7194815f630a518fd560ef5716e8019dcdc3e5
LiorKfir <liorkfir95@gmail.com>
2022-04-01 16:35:19 +0300

    new

---
+++ Button.py
@@ -0,0 +1,22 @@
+# from helpers import *
+# from Constants import *
+# from helpers import *
+#
+#
+# class Button:
+#     def __init__(self, x_pos, y_pos, width, height):
+#         self.x_pos = x_pos
+#         self.y_pos = y_pos
+#         self.width = width
+#         self.height = height
+#
+#
+# Play_BUTTON = Button(PLAY_BUTTON_X_POS, PLAY_BUTTON_Y_POS, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HIGHT)
+#
+#
+# def display_button(self):
+#     screen.blit(self.Play_BUTTON, (PLAY_BUTTON_X_POS, PLAY_BUTTON_Y_POS))
+
+
+
+
